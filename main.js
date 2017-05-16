655b1014d69f85177fb62f52fa47e36d6f855e66
HaseYo4416 <code_name_c2000@yahoo.co.jp>
2017-05-16 13:22:02 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -13,22 +13,41 @@ function main()
     camera.position.set( 0, 0, 5 );
     scene.add( camera );
 
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    loop();
-
-    function loop()
-    {
-        requestAnimationFrame( loop );
-        cube.rotation.x += 0.001;
-        cube.rotation.y += 0.001;
-        renderer.render( scene, camera );
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    // var material = new THREE.ShaderMaterial({
+    //     vertexColors: THREE.VertexColors,
+    //     vertexShader: document.
+    //         getElementById('shader.vert').text,
+    //     fragmentShader: document.getElementById('shader.frag').text,
+    // });
+var material = new THREE.ShaderMaterial({
+    vertexColors: THREE.VertexColors,
+    vertexShader: document.getElementById('phong.vert').text,
+    fragmentShader: document.getElementById('phong.frag').text,
+    uniforms: {
+light_position: { type: 'v3', value: light.position }
     }
+});
+
+
+var torus_knot = new THREE.Mesh( geometry, material );
+scene.add( torus_knot );
+
+loop();
+
+function loop()
+{
+    requestAnimationFrame( loop );
+    torus_knot.rotation.x += 0.01;
+    torus_knot.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
 }
